ef514d5b9b4bf374c7171f51cbe52aa21c859d5b
choyccc <choyccc@gmail.com>
2022-09-28 15:15:24 +0900

    fourth

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static("build")); // 빌드 폴더 접근 권한 설정
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/build/index.html"); // root 경로를 의미하는 __dirname
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server is running");
 });
